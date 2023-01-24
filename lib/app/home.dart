22b4fd5ad7ebbed50d5f2379e1ff1d32f734b556
ElcioLS <elciols@gmail.com>
2023-01-23 22:27:45 -0300

    Provider e CustomDio implements

--- lib/app/home.dart
+++ lib/app/home.dart
@@ -1,6 +1,7 @@
 import 'package:adf_dw9_delivery/app/core/ui/theme/theme_config.dart';
 import 'package:flutter/material.dart';
 
+import 'core/provider/application_binding.dart';
 import 'pages/splash/splash_page.dart';
 
 class Home extends StatelessWidget {
@@ -8,13 +9,15 @@ class Home extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Delivery App',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeConfig.theme,
-      routes: {
-        '/': (context) => const SplashPage(),
-      },
+    return ApplicationBinding(
+      child: MaterialApp(
+        title: 'Delivery App',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeConfig.theme,
+        routes: {
+          '/': (context) => const SplashPage(),
+        },
+      ),
     );
   }
 }
